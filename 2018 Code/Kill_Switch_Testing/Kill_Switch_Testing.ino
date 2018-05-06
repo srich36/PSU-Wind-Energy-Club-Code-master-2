77245ace0ec1de66d11a6ecb3a450e0f5dafec98
Sean Rich <srich366@gmail.com>
2018-05-06 19:13:28 -0400

    Update Load Code and kill switch testing

--- 2018 Code/Kill_Switch_Testing/Kill_Switch_Testing.ino
+++ 2018 Code/Kill_Switch_Testing/Kill_Switch_Testing.ino
@@ -1,9 +1,12 @@
+#include <Servo.h>
 
+Servo pitch;
 void setup(){
   pinMode(A1, INPUT);
   pinMode(8, OUTPUT);
   Serial.begin(9600);
   Serial.println("Starting. ");
+  pitch.attach(9);
 }
 
 
@@ -11,16 +14,17 @@ void loop(){
   boolean killed = (digitalRead(A1) == LOW);
   double voltage = digitalRead(A1);
   Serial.print("Voltage equals: ");
-  if(digitalRead(A1) == HIGH){
-    Serial.println("Returned high");
+  if(digitalRead(A1) == LOW){
+    Serial.println("kill switch hit");
     Serial.println("Sending a high signal to the load Arduino.");
     digitalWrite(8, HIGH);
+    pitch.write(110);
     delay(1000);
   }
   else{
     Serial.println("Returned low");
+    pitch.write(45);
   }
-  Serial.println(voltage);
-  Serial.println(killed);
-  delay(1000);
+  
+  delay(2000);
 }
