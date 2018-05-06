4b810b78941cd5d8e443e537dcd47c195f0f7a03
Sean Rich <srich366@gmail.com>
2018-05-06 14:11:19 -0400

    Creates load sending power back test

---
+++ 2018 Code/Send_Power_Back_To_PCC_Test/Send_Power_Back_To_PCC_Test.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH);
+  //Change "HIGH" to "LOW" if you don't want to output 5V out of pin 3 on the load arduino
+}
